d4a10ea1c2333d888af793a7187e4b776e5b794a
madubashana-2004 <vibodhaherath@gmail.com>
2025-05-03 11:59:22 +0530

    py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -108,7 +108,8 @@ def login():
     additional_claims = {'role': user.role, 'password': data['password']}
 
     # Create access token and refresh token
-    access_token = create_access_token(identity=user.id, additional_claims=additional_claims)
+    access_token = create_access_token(identity=user.id, fresh=True, additional_claims=additional_claims)
+
     refresh_token = create_refresh_token(identity=user.id, additional_claims=additional_claims)
 
     response_data = {"message": "Login successful", "user": user.to_dict()}
